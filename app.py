8528734fa0aa33e541b977ba1d2784b85ff50b3d
Jarodzsc25 <cadizsalanovajarodzarian@gmail.com>
2025-10-17 17:00:47 -0400

    se jodio todo marino XD, tuve que usar git para reponer mi fallo de la vrg

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
-# ===== Importar extensiones =====
+# ===== Importar extensiones =====--
 from extensions import bcrypt
 
 # ===== Crear app =====
