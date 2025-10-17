fb2110b2d58bb133960d5218cf1b3cd9f9bb7589
Jarodzsc25 <cadizsalanovajarodzarian@gmail.com>
2025-10-17 14:23:07 -0400

    ya se probo y corre sin problemas

--- db.py
+++ db.py
@@ -10,21 +10,3 @@ DB_CONFIG = {
 
 def get_connection():
     return connect(**DB_CONFIG)
-
-
-
-# import psycopg2
-#
-# def get_connection():
-#     try:
-#         connection = psycopg2.connect(
-#             host="localhost",
-#             database="ventas",
-#             user="postgres",
-#             password="latorrededruaka",
-#             port="5432"
-#         )
-#         return connection
-#     except Exception as e:
-#         print("Error al conectar a la base de datos:", e)
-#         return None
